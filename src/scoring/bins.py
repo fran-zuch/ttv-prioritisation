40c0e7d53316705d68558db1411a107baa5b2e51
fran-gipane <franny@tutamail.com>
2026-06-24 16:36:54 +0100

    Refactor bin_instrument function for scoring

--- src/scoring/bins.py
+++ src/scoring/bins.py
@@ -7,10 +7,14 @@ def bin_observability(x):
 def bin_ttv(x):
     return 1 if x < 2 else 2 if x < 5 else 3 if x < 10 else 4 if x < 20 else 5
 
-def bin_instrument(mag, depth):
-    mag_score = 5 if mag < 10 else 4 if mag < 11 else 3 if mag < 12 else 2 if mag < 13 else 1
-    depth_score = 5 if depth > 10 else 4 if depth > 5 else 3 if depth > 2 else 2 if depth > 1 else 1
-    return int((mag_score + depth_score) / 2)
+def bin_instrument(x):
+    return (
+        1 if x <= 0 else
+        2 if x == 1 else
+        3 if x == 2 else
+        4 if x <= 4 else
+        5
+    )
 
 def bin_synergy(x):
     return (
